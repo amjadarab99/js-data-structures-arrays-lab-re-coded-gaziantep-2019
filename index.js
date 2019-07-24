c7350728699ccf313a261da21e6cf1ce157bbf6e
Amjad Arab <amjad.arab99@gmail.com>
2019-07-24 19:16:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,20 @@ function destructivelyRemoveLastDriver(){
 function destructivelyRemoveFirstDriver(){
  drivers.shift();
 }
-
+const drivers = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendDriver(){
+ drivers.push("Ralph");
+}
+function destructivelyPrependDriver(){
+ drivers.unshift("Bob");
+}
+function destructivelyRemoveLastDriver(){
+ drivers.pop();
+}
+function destructivelyRemoveFirstDriver(){
+ drivers.shift();
+}
+function appendDriver(name){
+ name = [...drivers,"Broom"];
+ return name;
+}
