f9426ebf2d348e0708b4a8946873b635c9ab7216
Amjad Arab <amjad.arab99@gmail.com>
2019-07-24 19:16:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Write your solution here!
-const drivers=["Milo", "Otis", "Garfield"];
-
+const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(){
  drivers.push("Ralph");
 }
 function destructivelyPrependDriver(){
  drivers.unshift("Bob");
+}
+function destructivelyRemoveLastDriver(){
+ drivers.pop();
+}
+function destructivelyRemoveFirstDriver(){
+ drivers.shift();
 }
