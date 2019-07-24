2bf6be0de0b5c844fbfb29094d9c516bf57ea5e1
Amjad Arab <amjad.arab99@gmail.com>
2019-07-24 19:19:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,20 +12,17 @@ function destructivelyRemoveLastDriver(){
 function destructivelyRemoveFirstDriver(){
  drivers.shift();
 }
-const drivers = ["Milo", "Otis", "Garfield"];
-function destructivelyAppendDriver(){
- drivers.push("Ralph");
-}
-function destructivelyPrependDriver(){
- drivers.unshift("Bob");
-}
-function destructivelyRemoveLastDriver(){
- drivers.pop();
-}
-function destructivelyRemoveFirstDriver(){
- drivers.shift();
-}
 function appendDriver(name){
  name = [...drivers,"Broom"];
  return name;
+}
+function prependDriver(name){
+ name = ["Arnold",...drivers];
+ return name;
+}
+function removeLastDriver(){
+ return drivers.slice(0,drivers.length -1);
+}
+function removeFirstDriver(){
+ return drivers.slice(1,drivers.length);
 }
